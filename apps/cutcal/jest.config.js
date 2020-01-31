41c616f4d64b453a22c7d601201907bafdffddc1
nate <nathaniel.may22@gmail.com>
2020-01-30 21:10:38 -0600

    initial commit

---
+++ apps/cutcal/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'cutcal',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/cutcal',
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js'
+  ]
+};
