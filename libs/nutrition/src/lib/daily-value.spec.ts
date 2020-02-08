a8bf7441421f627f68a2a72c32695137828a6921
nate <nathaniel.may22@gmail.com>
2020-02-07 21:19:49 -0600

    hucky and linters installed

--- libs/nutrition/src/lib/daily-value.spec.ts
+++ libs/nutrition/src/lib/daily-value.spec.ts
@@ -1,28 +1,27 @@
 import { createNutritionRange } from './daily-value';
 
 describe('NutritionRange class', () => {
-
   it('should set from RDA and UL if provided', () => {
-    const result = createNutritionRange(4, 30, 'g', 10, 60)
-    
+    const result = createNutritionRange(4, 30, 'g', 10, 60);
+
     expect(result).toEqual({
       unit: 'g',
       EAR: 4,
       AI: 30,
       UL: 60,
-      RDA: 10
-    })
-  })
+      RDA: 10,
+    });
+  });
 
   it('should calulate high and low i', () => {
-    const result = createNutritionRange(10, 30, 'g')
-    
+    const result = createNutritionRange(10, 30, 'g');
+
     expect(result).toEqual({
       unit: 'g',
       EAR: 10,
       AI: 30,
       UL: 26,
-      RDA: 14
-    })
-  })
-})
+      RDA: 14,
+    });
+  });
+});
