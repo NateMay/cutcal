c0e0675cd1683139aa9c29688d118210c4e234dd
nate <nathaniel.may22@gmail.com>
2020-02-07 22:32:05 -0600

    testing nx affected only3

--- apps/common-ui/src/app/app.component.ts
+++ apps/common-ui/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-  selector: 'cutcal-root',
+  selector: 'cc-common-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
