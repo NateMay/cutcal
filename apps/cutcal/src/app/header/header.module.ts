39d63b77cd417d2c3a5c784802baabbd0ee3c9b8
nate <nathaniel.may22@gmail.com>
2020-02-08 12:18:32 -0600

    code coverage script + sign in btn

--- apps/cutcal/src/app/header/header.module.ts
+++ apps/cutcal/src/app/header/header.module.ts
@@ -1,11 +1,12 @@
 import { OverlayModule } from '@angular/cdk/overlay'
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
 import { HeaderComponent } from './header.component'
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, OverlayModule],
+  imports: [CommonModule, OverlayModule, MatButtonModule],
 })
 export class HeaderModule {}
