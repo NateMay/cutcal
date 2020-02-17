68f32471e075d04e6e3c94ee9fa34aea072bf0a0
nate <nathaniel.may22@gmail.com>
2020-02-16 22:28:34 -0600

    removed jest-createSpyObj

--- libs/common/src/lib/functions/executeOnStable/executeOnStable.spec.ts
+++ libs/common/src/lib/functions/executeOnStable/executeOnStable.spec.ts
@@ -1,11 +1,6 @@
 import { EventEmitter } from '@angular/core';
-import { createSpyObj } from '@cutcal/ng-testing';
 import { executeOnStable } from './executeOnStable';
 
-export const mockNgZone = createSpyObj('mockNgZone', [
-  'run',
-  'runOutsideAngular',
-]);
 export class MockNgZone {
   onStable = new EventEmitter<any>();
   runOutsideAngular(func) {
