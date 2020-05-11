a4bc5e94b9489e3b6e3eeb842577a505f8676ed1
nate <nathaniel.may22@gmail.com>
2020-05-10 23:08:08 -0500

    removed fdc lib in favor of nestjs service

--- apps/functions/src/nest-api/fdc.service.ts
+++ apps/functions/src/nest-api/fdc.service.ts
@@ -19,4 +19,95 @@ export class FdcService {
   // async scrapeDescription(link: string): Promise<Message> {
   //   return { message: pluckEarlyParagraphs(await rp(link)) }
   // }
+
+  // getFdcFood(fdcId: number | string, uid?: string): Observable<Food2> {
+  //   // ?api_key=${this.apiKey}
+  //   return this.http
+  //     .get<FdcFoodDetailResponse>(`${this.endPoint2}/${fdcId}`)
+  //     .pipe(
+  //       map((response: FdcFoodDetailResponse) =>
+  //         createFood2(
+  //           response.description,
+  //           this.nutritionFromResponse(response),
+  //           response.description,
+  //           response.id,
+  //           response.description,
+  //           this.portionsFromResponse(response),
+  //           createPortion('grams', 100),
+  //           uid,
+  //           this.tagsFromResponse(response),
+  //           {},
+  //           {}
+  //         )
+  //       ),
+  //       shareReplay()
+  //     )
+  // }
+
+  // queryFood(
+  //   searchStr: string,
+  //   page: number = 1
+  // ): Observable<FDCFoodSearchResponse> {
+  //   // https://DEMO_KEY@api.nal.usda.gov/fdc/v1/search
+  //   return this.http.post<FDCFoodSearchResponse>(
+  //     'https://fdc.nal.usda.gov/portal-data/external/search',
+  //     {
+  //       exactBrandOwner: null,
+  //       generalSearchInput: searchStr,
+  //       includeDataTypes: {
+  //         'Survey (FNDDS)': true,
+  //         Foundation: true,
+  //         Branded: false,
+  //         'SR Legacy': true
+  //       },
+  //       pageNumber: page.toString(),
+  //       referenceFoodsCheckBox: 'true',
+  //       sortCriteria: {
+  //         sortColumn: 'description',
+  //         sortDirection: 'asc'
+  //       }
+  //     }
+  //   )
+  // }
+
+  // private nutritionFromResponse(
+  //   response: FdcFoodDetailResponse
+  // ): Nutrition<number> {
+  //   const base: KVP<number> = {}
+  //   const details = keyBy(NUTRIENTS.allDetails, 'id')
+  //   response.foodNutrients.forEach((foodNutrient: FdcFoodNutrient) => {
+  //     const meta = details[foodNutrient.nutrient.id]
+  //     if (meta?.propName) base[meta.propName] = foodNutrient.value || 0
+  //   })
+  //   return base
+  // }
+
+  // private portionsFromResponse(response: FdcFoodDetailResponse): KVP<Portion> {
+  //   return {
+  //     grams: {
+  //       unit: 'grams',
+  //       quantity: 100
+  //     },
+  //     ...keyBy(
+  //       response.foodMeasures ||
+  //         [].map((portion: FdcFoodMeasure) =>
+  //           createPortion(
+  //             this.unitFromMeasure(portion),
+  //             portion.gramWeight / 100
+  //           )
+  //         ),
+  //       'unit'
+  //     )
+  //   }
+  // }
+
+  // private unitFromMeasure(portion: FdcFoodMeasure): string {
+  //   return portion.disseminationText
+  //     ? portion.disseminationText.replace(/^(1 )/, '')
+  //     : portion.modifier
+  // }
+
+  // private tagsFromResponse(response: FdcFoodDetailResponse): string[] {
+  //   return response.foodAttributes.map(att => att.value)
+  // }
 }
--- libs/fdc/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'fdc',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/fdc',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
-}
